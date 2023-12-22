fc9d7a0342f9d19f8a31eabd2f5cfcaf0ba01e5a
Princess Genevieve Sagrado <xhanhime@gmail.com>
2023-12-22 22:30:14 +0800

    adjusted but not done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState, useRef }  from "react";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
-import { Button, Dropdown, Menu } from "antd";
+import { Dropdown, Menu } from "antd";
 import {
   NotificationImportant,
   Notifications,
   Person,
+  ViewHeadline,
+  WidgetsOutlined,
+  Login,
 } from "@mui/icons-material";
 import logo from "./assets/logo-blue.jpg";
 import "./App.css";
 import { useAuth } from "./AuthContext";
+import { Button } from "@mui/material";
 
 function Navbar(props) {
   const { user, logout } = useAuth();
+  const contSelectionRef = useRef(null);
+
+  const handleMenu = () => {
+    const contSelection = contSelectionRef.current;
+
+    if (contSelection) {
+      contSelection.classList.toggle("active");
+    }
+  };
 
   const ProfileMenu = (
     <Menu style={{ width: 250 }}>
@@ -25,58 +38,66 @@ function Navbar(props) {
   );
 
   return (
-    <nav className="nav">
-      <div className="nav-header">
-        <img className="nav-logo" src={logo} alt="Logo" />
-        <div className="title-nav">
-          <h3 className="nav-h3">UNIVERSITY OF SCIENCE AND TECHNOLOGY</h3>
-          <h3 className="nav-h3">OF SOUTHERN PHILIPPINES</h3>
+    <>
+      <nav className="nav">
+        <div className="nav-header">
+          <img className="nav-logo" src={logo} alt="Logo" />
+          <div className="title-nav">
+            <h3 className="nav-h3">UNIVERSITY OF SCIENCE AND TECHNOLOGY</h3>
+            <h3 className="nav-h3">OF SOUTHERN PHILIPPINES</h3>
+          </div>
         </div>
-      </div>
 
-      {user && (
-        <ul className="ui-nav">
-          {user.role === "admin" ? (
-            <>
-              <CustomLink to="/dashboardadmin">Dashboard</CustomLink>
-              <CustomLink to="/adduser">Add User</CustomLink>
-              <CustomLink to="/userlist">User List</CustomLink>
-            </>
-          ) : (
-            <>
-              <CustomLink to="/dashboard">Dashboard</CustomLink>
-              <CustomLink to="/service">Service</CustomLink>
-              <CustomLink to="/download">Downloadable Files</CustomLink>
-              <CustomLink to="/submit">Submit Request</CustomLink>
-            </>
-          )}
-        </ul>
-      )}
+        {user && (
+          <ul className="ui-nav">
+            {user.role === "admin" ? (
+              <>
+                <CustomLink to="/dashboardadmin">Dashboard</CustomLink>
+                <CustomLink to="/adduser">Add User</CustomLink>
+                <CustomLink to="/userlist">User List</CustomLink>
+              </>
+            ) : (
+              <>
+                <CustomLink to="/dashboard">Dashboard</CustomLink>
+                <CustomLink to="/service">Service</CustomLink>
+                <CustomLink to="/download">Downloadable Files</CustomLink>
+                <CustomLink to="/submit">Submit Request</CustomLink>
+              </>
+            )}
+          </ul>
+        )}
 
-      <li className="logout">
-        <div className="logout-con bell">
-          <Button
-            style={{
-              backgroundColor: "transparent",
-              color: "white",
-              border: "none",
-            }}
-          >
-            <Notifications />
-          </Button>
-        </div>
-        <div className="logout-con">
-          <Dropdown
-            overlay={ProfileMenu}
-            placement="bottomRight"
-            arrow
-            trigger={["click"]}
-          >
-            <Person style={{ color: "white" }} />
-          </Dropdown>
-        </div>
-      </li>
-    </nav>
+        <li className="logout">
+          <div className="logout-con bell">
+            <Button
+              style={{
+                backgroundColor: "transparent",
+                color: "white",
+                border: "none",
+              }}
+            >
+              <Notifications />
+            </Button>
+          </div>
+          <div className="logout-con drop">
+            <Dropdown
+              overlay={ProfileMenu}
+              placement="bottomRight"
+              arrow
+              trigger={["click"]}
+            >
+              <Person style={{ color: "white" }} />
+            </Dropdown>
+          </div>
+          <div className="logout-con bar">
+            <Button onClick={handleMenu}>
+              <ViewHeadline style={{ color: "white" }}></ViewHeadline>
+            </Button>
+          </div>
+        </li>
+      </nav>
+      <div ref={contSelectionRef} className="drop-down-nav"></div>
+    </>
   );
 }
 
