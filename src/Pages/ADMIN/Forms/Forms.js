3557694e424e18e8167ecdb246e80956f8192582
PRINCESS\sprin <sprincessgenevieve@gmail.com>
2024-01-30 16:38:43 +0800

    Fixed Login

---
+++ src/Pages/ADMIN/Forms/Forms.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../../../Navbar";
+
+export default function FormApp() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>FORM APPLICATION</h1>
+    </div>
+  );
+}
